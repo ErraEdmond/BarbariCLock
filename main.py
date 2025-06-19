b6582bc951fb08c953313bcfc566b39b5a98cadb
Erra Edmond <ave.lerp@gmail.com>
2025-06-19 10:51:42 +0300

    Core works! All that's left is to make a GUI and add a break section.

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-from time import localtime, gmtime, sleep
+from time import localtime, sleep
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
-from threading import Thread, active_count
+from threading import Thread, active_count 
 
 import sys 
-# while True: 
-#     time = localtime()
-#     print(f'{time[3]}:{time[4]}')
-#     sleep(60)
 
 class MyWindow(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.min = 0
-        self.sec = 1
+        self.timer_deployment()
+        self.paused = False
+        self.stopped = False 
         self.iniUI()
+
+
+    def timer_deployment(self): 
+        self.min = 25
+        self.sec = 0
 
 
     def iniUI(self): 
@@ -28,47 +30,73 @@ class MyWindow(QWidget):
         self.label.setText(f'{self.min} : {self.sec}')
         self.label.setAlignment(Qt.AlignVCenter)
 
-        self.start_button = QPushButton('Push', self)
-        self.start_button.clicked.connect(self.ti_thread)
+        self.start_button = QPushButton('Start', self)
+        self.start_button.clicked.connect(lambda: self.ti_thread(tt = True))
+        self.pause_button = QPushButton('Pause', self)
+        self.pause_button.clicked.connect(lambda: self.regulations(p=True, s=False)) 
+        self.stop_button = QPushButton('Stop', self)
+        self.stop_button.clicked.connect(lambda: self.regulations(p=False, s=True)) 
+
 
         layout = QVBoxLayout()
         layout.addWidget(self.label)
         layout.addWidget(self.start_button)
+        layout.addWidget(self.pause_button)
+        layout.addWidget(self.stop_button)
         self.setLayout(layout)
 
         self.show()
+        self.ti_thread(tt = False)
         self.cl_thread()
+
+
+    def regulations(self, p : bool, s : bool):
+        if p: 
+            self.paused = bool(1 - self.paused)
+        if s: 
+            self.stopped = bool(1 - self.stopped)
 
 
     def timer(self):
-        while self.isVisible() and self.min >= 0: 
-            if self.sec < 0: 
+        print(active_count())
+        while self.isVisible():
+            if self.min < 0 or self.stopped:
+                break
+
+            if self.sec == 0: 
                 self.min -= 1
                 self.sec = 59
-            self.label.setText(f'{self.min}:{str(self.sec).zfill(2)}')
-            self.sec -= 1
+
             sleep(1)
+            if self.paused == True:
+                continue
+            self.label.setText(f'{self.min:02d}:{self.sec:02d}')
+            self.sec -= 1
+        
+        self.stopped = False 
+        self.timer_deployment()
+        self.clock()
+
+
+    def ti_thread(self, tt : bool):
+        th_timer = Thread(target=self.timer)
+        if th_timer.is_alive == True: 
+            self.timer()
+        if tt and th_timer.is_alive != True:
+            th_timer.start()
+            th_clock.join()
 
 
     def clock(self):
-        print(active_count())
-        while self.isVisible() and active_count() <= 2:
-            self.label.setText(f'{localtime()[3]} : {localtime()[4]} : {localtime()[5]}')
+        while self.isVisible() and active_count() < 3:
+            self.label.setText(f'{localtime()[3]:02d} : {localtime()[4]:02d} : {localtime()[5]:02d}')
             sleep(1)
 
 
-    def cl_thread(self): 
-        t1 = Thread(target=self.clock)
-        t1.start()
-    
-    def ti_thread(self): 
-        t1 = Thread(target=self.timer)
-        print(active_count())
-        t1.start()
-        self.cl_thread()
-        # looks like I have to find a way to eliminate thread after the end of function.
-        # I guess, thread-based solution is temporary. If I would ever create more functionality, I have to add emits 
-        # for timer + clock
+    def cl_thread (self): 
+        global th_clock
+        th_clock = Thread(target=self.clock)
+        th_clock.start()
 
 app = QApplication([])    
 win = MyWindow()
